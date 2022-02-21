3bc2ac6cd13a3cfcf9db5e871c8552ae21dec772
RobertoJuchem <juchemroberto@gmail.com>
2022-02-21 09:03:15 -0300

    Fixar o aside e o header

--- src/components/Content/style.ts
+++ src/components/Content/style.ts
@@ -4,5 +4,7 @@ export const Container = styled.div`
    background-color: ${props => props.theme.colors.primary};
    color: ${props => props.theme.colors.white};
    grid-area: CT;
+   height: calc(100vh - 70px);
+   overflow-y: scroll;
    padding: 25px;
 `
